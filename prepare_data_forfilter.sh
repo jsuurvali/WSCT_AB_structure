29d3fb095ee7d57024f97466e8570c6f05ec12c3
Jaanus Suurväli <8995770+jsuurvali@users.noreply.github.com>
2025-01-15 20:44:46 -0600

    Update prepare_data_forfilter.sh

--- prepare_data_forfilter.sh
+++ prepare_data_forfilter.sh
@@ -13,8 +13,10 @@ awk '$5 < 0.25 {print $1}' tmp.imiss | \
         bcftools view -S - wsct.snps.var.mac3.vcf.gz | \
         bcftools view -m2 -M2 -v snps -e 'MAC < 3 | F_MISSING > 0.25' -Oz -o wsct.snps.var.mac3.miss25.vcf.gz
 
+# write out the list of individuals in the file
 bcftools query -l wsct.snps.var.mac3.miss25.vcf.gz > keeplist.miss25
 
 rm tmp*
 
+# sample30.txt contaains sample name and population for selected populations with n >= 30
 cut -f1 sample30.txt | bcftools view -S - wsct.snps.var.mac3.miss25.vcf.gz -Oz -o forfilter.vcf.gz
